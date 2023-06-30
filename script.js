0d2a4fbbe007cb012514bfdf0b8aaf3b90ba4871
Nicolas Brival <99988248+NicolasBrival@users.noreply.github.com>
2023-06-30 09:20:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+/*
+
+const MoonSunElement = document.querySelector("#moonCircle")
+const backgroundContainerElement = document.querySelector("#container")
+
+MoonSunElement.addEventListener("click", handelPixelCLick)
+
+function handelPixelCLick(event) {
+    const ClickedMoonSun = event.currentTarget; //élément cliqué
+    backgroundContainerElement.style.animation= "day-night-move 6000ms ease-in";
+    console.log("Vous avez cliqué sur la lune !")
+}
+
+*/
