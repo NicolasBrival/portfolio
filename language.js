13d7016642bac551cc8ffb1718bcfaad93c65999
Nicolas Brival <nicolas.brival@oclock.school>
2023-07-03 13:14:08 +0200

    20230703v1

---
+++ language.js
@@ -0,0 +1,20 @@
+/*
+<li><a class="navbar-menu__element" href="index.html"><strong>Acceuil</strong></a></li>
+<li><a class="navbar-menu__element" href="#">Présentation</a></li>
+<li><a class="navbar-menu__element" href="#">Projets</a></li>
+<li><a class="navbar-menu__element" href="contact-page.html">Contact</a></li>
+</ul>
+</nav>
+<div id="active-elements">
+</div>
+
+</header>
+
+<main>
+
+<section id="presentation">
+<p>Hello,<br>
+I'm Nicolas, <strong>a Full Stack web and mobile Developper :<br>
+specialised in frontend and backend.</strong>
+</p>
+*/
